b64c065567e6bf774ee397d339c69522902ffec1
bhvphuc <buuhuynhvinhphuc@gmail.com>
2025-09-15 00:04:41 +0700

    Front end static

---
+++ to_do_app/src/App.js
@@ -0,0 +1,55 @@
+// eslint-disable-next-line
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+  return (
+    <div className="App">
+      <h1> My Todos</h1>
+
+      <div className ='todo-wrapper'>
+        <div className = 'todo-input'>
+
+          <div className = 'todo-input-item'>
+            <label>Title</label>
+            <input type="text" placeholder="What's the task title?"/>
+          </div>
+
+          <div className = 'todo-input-item'>
+            <label>description</label>
+            <input type="text" placeholder="What's the task description?"/>
+          </div>
+
+          <div className = 'todo-input-item'>
+            <button type='button' className='primaryBtn'>Add</button>
+          </div>
+
+        </div>
+        
+          <div className='btn-area'>
+            <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} 
+              onClick={()=>setIsCompleteScreen(false)}>Todo
+            </button>
+
+            <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`}  
+              onClick={()=>setIsCompleteScreen(true)}>Complete
+            </button>
+          </div>
+
+          <div className='todo-list'>
+
+            <div className='todo-list-item'>
+              <h3>Task 1</h3>
+              <p>Description</p>
+            </div>
+
+          </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
